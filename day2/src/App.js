4d58ccc0ac0af6f271a7a8e006710d141dd62769
Vigneshprabu1398 <76090127+Vigneshprabu1398@users.noreply.github.com>
2022-03-03 22:03:57 +0530

    new file

--- day2/src/App.js
+++ day2/src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+  const mobile = ["Android", "Black Berry", "iPhone", "Windows phone"];
+  const brand = ["samsung", "htc", "iPhone", "nokia"]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Mobile Operating System</h1>
+      <ul>{
+       mobile.map ((e)=>(
+          <li>{e}</li>
+        ))}
+      </ul>
+      <h1>Mobile Manufacturers</h1>
+      <ul>{
+       brand.map ((e)=>(
+          <li>{e}</li>
+        ))}
+      </ul>
     </div>
   );
+
 }
 
 export default App;
