1e2fb07b480e66fb415f5ab127ad5a5837320c04
Vigneshprabu1398 <76090127+Vigneshprabu1398@users.noreply.github.com>
2022-03-04 22:22:30 +0530

    new file

--- day2/src/App.js
+++ day2/src/App.js
@@ -1,27 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import {Counter} from './components/Counter.jsx'
 
 
 
 function App() {
-  const mobile = ["Android", "Black Berry", "iPhone", "Windows phone"];
-  const brand = ["samsung", "htc", "iPhone", "nokia"]
+  // const mobile = ["Android", "Black Berry", "iPhone", "Windows phone"];
+  // const brand = ["samsung", "htc", "iPhone", "nokia"]
 
-  return (
-    <div className='App'>
-      <h1>Mobile Operating System</h1>
-      <ul>{
-       mobile.map ((e)=>(
-          <li>{e}</li>
-        ))}
-      </ul>
-      <h1>Mobile Manufacturers</h1>
-      <ul>{
-       brand.map ((e)=>(
-          <li>{e}</li>
-        ))}
-      </ul>
-    </div>
+  // return (
+  //   <div className='App'>
+  //     <h1>Mobile Operating System</h1>
+  //     <ul>{
+  //      mobile.map ((e)=>(
+  //         <li>{e}</li>
+  //       ))}
+  //     </ul>
+  //     <h1>Mobile Manufacturers</h1>
+  //     <ul>{
+  //      brand.map ((e)=>(
+  //         <li>{e}</li>
+  //       ))}
+  //     </ul>
+  //   </div>
+  // );
+  return(
+    <>
+    <Counter value={30}/>
+    </>
   );
 
 }
