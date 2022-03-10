946cfd5aa88be95fb4cb1109ded6966a728c78d4
Vigneshprabu1398 <76090127+Vigneshprabu1398@users.noreply.github.com>
2022-03-10 15:22:05 +0530

    updated

---
+++ form/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [form,setForm] = useState({
+    age:"",
+    name:"",
+  });
+  const handleChange = (e) =>{
+    console.log(e.target);
+  };
+
+  return (
+    <div>
+      <form>
+        <h3>Form</h3>
+        <input
+        id="name"
+        type="text"
+        onChange ={handleChange}
+        placeholder="Enter Name"
+        />
+        <input
+        id="age"
+        type="number"
+        onChange ={handleChange}
+        placeholder="Enter Age"
+        />
+      </form>
+      console.log("object");
+    </div>
+  );
+}
+
+export default App
